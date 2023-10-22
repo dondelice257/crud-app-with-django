bfeee1b8b0d4d5901f1360e06fb4c657e9acc136
dondelice257 <dondelicedushime@gmail.com>
2023-10-22 17:45:00 +0200

    add links to student's details

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.students, name='students'),
     path('dropouts', views.dropouts, name='dropouts'),
-    path('students/details/<int:id>', views.students_details, name='students_details'),
+    path('details/<int:id>', views.students_details, name='students_details'),
     
 ]
